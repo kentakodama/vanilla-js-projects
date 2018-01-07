467ddad7adaabb733bd37257388c6cc9b71b384b
Kenta Kodama <kentakodama@gmail.com>
2018-01-06 22:13:14 -0800

    created volume and speed range handlers

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,5 +1,5 @@
 
-// grab all elements from the DOM
+// 1 grab all elements from the DOM
 const player = document.querySelector('.player');
 
 const video = player.querySelector('.viewer');
@@ -7,10 +7,10 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress_filled');
 
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
+const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelector('.player__slider')
 
-// build all functions
+// 3 build all functions
 
 function togglePlay() {
   //paused is a property on video
@@ -28,10 +28,44 @@ function updateButton() {
   console.log(this);
 }
 
-// hook up event listeners
-video.addEventListener('click', togglePlay)
-//interesting!
-video.addEventListener('play', updateButton)
-video.addEventListener('pause', updateButton)
+function skip() {
 
-toggle.addEventListener('click', togglePlay)
+  //video has currentTime attr
+  //update that time by the data built into skip buttons
+  video.currentTime += parseFloat(this.dataset.skip);
+
+}
+
+//this handles the volume and speeds
+function handleRangeUpdate() {
+  //this refers to range
+  //dynamically handles both the volume and speed
+  video[this.name] = this.value
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`
+}
+
+
+// 2 hook up event listeners
+video.addEventListener('click', togglePlay);
+//interesting!
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', handleProgress);
+
+skipButtons.forEach((button) => {
+  button.addEventListener('click', skip)
+});
+
+ranges.forEach((range) => {
+  range.addEventListener('change', handleRangeUpdate)
+});
+//this updates in real time
+ranges.forEach((range) => {
+  range.addEventListener('mousemove', handleRangeUpdate)
+});
