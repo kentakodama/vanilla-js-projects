cd8e32544439ebee7afde1ecfd72ad567952b2fa
Kenta Kodama <kentakodama@gmail.com>
2018-01-06 21:36:50 -0800

    extracted all elements from the dom

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,16 @@
+
+// grab all elements from the DOM
+const player = document.querySelector('.player');
+
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress_filled');
+
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('[data-skip]');
+const ranges = player.querySelector('.player__slider')
+
+// build all functions
+
+
+// hook up event listeners
